900d5cf8423f777b59c32ca318fb160f7154b414
Johnny <joh059@ucsd.edu>
2026-05-05 17:42:01 -0700

    testing branch: should be wrong

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
 
 Name: Johnny Huang
+
+## Explore Pt. 3 Questions
+
+1. I don't think that a unit test testing the "message" feature of a messaging application would be a good idea, since that feature is basically the entire premise of the application. There are a lot of smaller things about a message that would need to be tested, like if it delivers the message, can you actually type it out, etc.
+2. I think using a unit test for the "max length message" is a good test, since you can reasonably test this one part of the main feature without messing up other parts of the application.
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,66 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  // Defines all the voices (taken from the documentation site)
+  function populateVoiceList() {
+    if (typeof speechSynthesis === "undefined") {
+      return;
+    }
+
+    const voices = speechSynthesis.getVoices();
+
+    for (const voice of voices) {
+      const option = document.createElement("option");
+      option.textContent = `${voice.name} (${voice.lang})`;
+
+      if (voice.default) {
+        option.textContent += " — DEFAULT";
+      }
+
+      option.setAttribute("data-lang", voice.lang);
+      option.setAttribute("data-name", voice.name);
+      document.getElementById("voice-select").appendChild(option);
+    }
+  }
+
+  populateVoiceList();
+  if (
+    typeof speechSynthesis !== "undefined" &&
+    speechSynthesis.onvoiceschanged !== undefined
+  ) {
+    speechSynthesis.addEventListener("voiceschanged", populateVoiceList);
+  }
+
+  // Make the text to be spoken
+  let speakText = document.getElementById("text-to-speak");
+
+  let utterance = new SpeechSynthesisUtterance("");
+
+  speakText.addEventListener('input', function() {
+    utterance = new SpeechSynthesisUtterance(event.target.value);
+  });
+
+  // Pick the voice
+  let voiceSelect = document.getElementById("voice-select");
+  let selectedVoice = null;
+
+  voiceSelect.addEventListener("change", function() {
+    let selectedName = voiceSelect.selectedOptions[0].getAttribute("data-name");
+    selectedVoice = speechSynthesis.getVoices().find(v => v.name === selectedName);
+  });
+
+  // Make the play button make audio and image change
+  let talkButton = document.querySelector("button");
+  let mouthImg = document.querySelector("img");
+
+  talkButton.addEventListener("click", function() {
+    mouthImg.src = "assets/images/smiling-open.png";
+
+    utterance.onend = function() {
+      mouthImg.src = "assets/images/smiling.png";
+    };
+
+    utterance.voice = selectedVoice;
+    speechSynthesis.speak(utterance);
+  });
 }
